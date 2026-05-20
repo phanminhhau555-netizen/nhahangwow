75b0aa498fd0462b8205b0174a41925396b33f70
phanminhhau555-netizen <phanminhhau555@gmail.com>
2026-05-20 11:36:07 +0700

    bep

--- restaurant-frontend/src/App.jsx
+++ restaurant-frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/admin/Dashboard";
 import Staff from "./pages/admin/Staff";
 import MenuPage from "./pages/admin/Menu";
+import KitchenPage from "./pages/admin/Kitchen";
+
+<Route path="/admin/kitchen" element={<PrivateRoute><KitchenPage /></PrivateRoute>} />
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" />;
@@ -16,9 +19,8 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/admin/staff" element={<PrivateRoute><Staff /></PrivateRoute>} />
         <Route path="/admin/menu" element={<PrivateRoute><MenuPage /></PrivateRoute>}/>
-        <Route path="/admin/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>
-          }
-        />
+        <Route path="/admin/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
+        <Route path="/admin/kitchen" element={<PrivateRoute><KitchenPage /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   );
